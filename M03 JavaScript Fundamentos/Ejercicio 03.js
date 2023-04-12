45c6cd380bb5fd98872d4d6d0feca8f4f3db1c2f
Seralvju <santoseaj7@hotmail.com>
2023-04-12 06:59:42 -0500

    Modulo 03 terminado

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,67 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   var rpta=false;
+   if (x===y){
+      rpta=true;
+   }
+   return rpta;
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   var rpta=false;
+   if(str1.length === str2.length){
+      rpta=true;
+   }
+
+   return rpta;
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   var rpta=false;
+   if(num<90){
+      rpta =true;
+   }
+   return rpta;
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   var rpta=false;
+   if(num>50){
+      rpta =true;
+   }
+   return rpta;
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   var rpta=false;
+   if(num%2===0){
+      rpta =true;
+   }
+   return rpta;
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   var rpta=false;
+   if(num%2!=0){
+      rpta =true;
+   }
+   return rpta;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
